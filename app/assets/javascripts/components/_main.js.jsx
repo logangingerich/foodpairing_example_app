8b726bdea143e9e00aba6ff5a9504c4c712d826a
Logan Gingerich <loganpgingerich@gmail.com>
2017-10-08 09:09:33 -0600

    More styling and UI changes

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -6,14 +6,37 @@ var Main = React.createClass({
   onClickWhiskey: function() {
     let resultsState = true;
     this.setState({ showResults: resultsState,
-                    pairings: this.props.whiskey_pairings
+                    pairings: this.props.whiskey_pairings,
+                    name: 'Bourbon Whiskey'
 
      });
   },
-  onClickOther: function() {
+  onClick2: function() {
     let resultsState = true;
     this.setState({ showResults: resultsState,
-                    pairings: 'hi'
+                    pairings: this.props.pairings_2,
+                    name: 'Apple'
+     });
+  },
+  onClick3: function() {
+    let resultsState = true;
+    this.setState({ showResults: resultsState,
+                    pairings: this.props.pairings_3,
+                    name: 'Smoked Atlantic Salmon'
+     });
+  },
+  onClick4: function() {
+    let resultsState = true;
+    this.setState({ showResults: resultsState,
+                    pairings: this.props.pairings_4,
+                    name: 'Asparagus'
+     });
+  },
+  onClick5: function() {
+    let resultsState = true;
+    this.setState({ showResults: resultsState,
+                    pairings: this.props.pairings_5,
+                    name: 'Artichoke'
      });
   },
   render() {
@@ -21,10 +44,15 @@ var Main = React.createClass({
       <div>
         <div className="button-container">
           <button type="button" className="btn btn-primary ind-btn" onClick={this.onClickWhiskey}>Bourbon Whiskey</button>
-          <button type="button" className="btn btn-primary ind-btn" onClick={this.onClickOther}>Other</button>
+          <button type="button" className="btn btn-primary ind-btn" onClick={this.onClick2}>Apple</button>
+          <button type="button" className="btn btn-primary ind-btn" onClick={this.onClick3}>Smoked Atlantic Salmon</button>
+          <button type="button" className="btn btn-primary ind-btn" onClick={this.onClick4}>Asparagus</button>
+          <button type="button" className="btn btn-primary ind-btn" onClick={this.onClick5}>Artichoke</button>
         </div>
         <div className="results">
-          <h1>{ this.state.showResults ? this.state.pairings : 'Click an Ingredient' }</h1>
+          <h2>{ this.state.showResults ? 'Try pairing these ingredients with ' : '' }{this.state.name}</h2>
+          <br></br>
+          <h3>{ this.state.showResults ? this.state.pairings : 'Click an ingredient to get started' }</h3>
         </div>
       </div>
     )
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,39 @@
 class WelcomeController < ApplicationController
   def index
-    @test = Foodpairing.get_all_ingredients
-    @test2 = Foodpairing.get_ingredient_by_id(1)
-    @test3 = Foodpairing.search_ingredients_by_name("whiskey")
+    #get_all_ingredient returns an object containing all ingredients with
+    #descriptions
+    @all_ingredients = Foodpairing.get_all_ingredients
 
-    @test4 = Foodpairing.get_pairings_for_ingredient(1)
-    @whiskey_pairings_array = []
-    @test4.each do |pairing|
-      @whiskey_pairings_array.push(pairing["_links"]["ingredient"]["name"])
-    end
+    #get_ingredient_by_id returns an object containing the corresponding
+    #ingredient along with a description
+    @ingredient_by_id = Foodpairing.get_ingredient_by_id(1)
+
+    # search_ingredients_by_name takes in a string and returns an object with
+    #all matching ingredients
+    @ingredient_by_name = Foodpairing.search_ingredients_by_name("whiskey")
+
+    #get_pairings_for_ingredient takes in an ingredient id and returns an
+    # ARRAY of ingredients that would pair well.
+    @whiskey_pairings_array = Foodpairing.get_pairings_for_ingredient(1)
     @whiskey_pairings = @whiskey_pairings_array.join("  -  ")
-    @test5 = Foodpairing.get_ingredient_nutrients(1)
-    @test6 = Foodpairing.get_all_brands
-    @test7 = Foodpairing.get_brand_by_id(114)
+    @pairings_array_2 = Foodpairing.get_pairings_for_ingredient(94)
+    @pairings_2 = @pairings_array_2.join("  -  ")
+    @pairings_array_3 = Foodpairing.get_pairings_for_ingredient(4281)
+    @pairings_3 = @pairings_array_3.join("  -  ")
+    @pairings_array_4 = Foodpairing.get_pairings_for_ingredient(5777)
+    @pairings_4 = @pairings_array_4.join("  -  ")
+    @pairings_array_5 = Foodpairing.get_pairings_for_ingredient(6176)
+    @pairings_5 = @pairings_array_5.join("  -  ")
+
+    #get_ingredient_nutrients takes in an ingredient id and returns an object
+    #containing links to the ingredients nutrients
+    @ingredient_nutrients = Foodpairing.get_ingredient_nutrients(1)
+
+    #get_all_brands returns an object containing the name of all brands
+    @all_brands = Foodpairing.get_all_brands
+
+    #get_brand_by_id takes in a brand id and returns on object containing
+    #information on the corresponding brand
+    @brand_by_id = Foodpairing.get_brand_by_id(114)
   end
 end
